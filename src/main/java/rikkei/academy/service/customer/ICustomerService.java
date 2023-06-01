13011c39f44a200842c951d4d849ea1238edb334
vangtt <giangthanhvan12@gmail.com>
2023-06-01 08:38:36 +0700

    customer management

---
+++ src/main/java/rikkei/academy/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package rikkei.academy.service.customer;
+
+import rikkei.academy.model.Customer;
+import rikkei.academy.repository.IGeneralRepository;
+
+public interface ICustomerService extends IGeneralRepository<Customer> {
+}
